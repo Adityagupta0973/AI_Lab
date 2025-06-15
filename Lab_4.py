14451cced8cf1f805d13bf1766101febbe91ad83
Adityagupta0973 <adityag0908@gmail.com>
2025-06-15 19:14:03 +0530

    Lab4 Commit

--- Lab_4.py
+++ Lab_4.py
@@ -1,100 +1,94 @@
 import heapq
-from termcolor import colored
 
-class PuzzleState:
-    def __init__(self, board, parent, move, depth, cost):
-        self.board = board  # The puzzle board configuration
-        self.parent = parent  # Parent state
-        self.move = move  # Move to reach this state
-        self.depth = depth  # Depth in the search tree
-        self.cost = cost  # Cost (depth + heuristic)
+# Goal configuration
+GOAL = [1, 2, 3,
+        4, 5, 6,
+        7, 8, 0]
 
-    def __lt__(self, other):
-        return self.cost < other.cost
-
-def print_board(board):
-    print("+---+---+---+")
-    for row in range(0, 9, 3):
-        row_visual = "|"
-        for tile in board[row:row + 3]:
-            if tile == 0:  # Blank tile
-                row_visual += f" {colored(' ', 'cyan')} |"
-            else:
-                row_visual += f" {colored(str(tile), 'yellow')} |"
-        print(row_visual)
-        print("+---+---+---+")
-
-goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-moves = {
-    'U': -3,  # Move up
-    'D': 3,   # Move down
-    'L': -1,  # Move left
-    'R': 1    # Move right
+# Directions: move offset in the flat list
+DIRECTIONS = {
+    'U': -3,
+    'D': 3,
+    'L': -1,
+    'R': 1
 }
 
-def heuristic(board):
-    distance = 0
-    for i in range(9):
-        if board[i] != 0:
+# Valid move constraints
+def is_valid_move(pos, move):
+    if move == 'U' and pos < 3: return False
+    if move == 'D' and pos > 5: return False
+    if move == 'L' and pos % 3 == 0: return False
+    if move == 'R' and pos % 3 == 2: return False
+    return True
+
+# Heuristic: Manhattan distance
+def manhattan(board):
+    dist = 0
+    for i, val in enumerate(board):
+        if val != 0:
+            goal_i = GOAL.index(val)
             x1, y1 = divmod(i, 3)
-            x2, y2 = divmod(board[i] - 1, 3)
-            distance += abs(x1 - x2) + abs(y1 - y2)
-    return distance
+            x2, y2 = divmod(goal_i, 3)
+            dist += abs(x1 - x2) + abs(y1 - y2)
+    return dist
 
-def move_tile(board, move, blank_pos):
+# Move function
+def move(board, direction, blank):
     new_board = board[:]
-    new_blank_pos = blank_pos + moves[move]
-    new_board[blank_pos], new_board[new_blank_pos] = new_board[new_blank_pos], new_board[blank_pos]
-    return new_board
+    delta = DIRECTIONS[direction]
+    new_blank = blank + delta
+    new_board[blank], new_board[new_blank] = new_board[new_blank], new_board[blank]
+    return new_board, new_blank
 
-def a_star(start_state):
-    open_list = []
-    closed_list = set()
-    heapq.heappush(open_list, PuzzleState(start_state, None, None, 0, heuristic(start_state)))
+# A* Search
+def a_star(start):
+    start_h = manhattan(start)
+    start_state = (start_h, 0, start, start.index(0), [])  # (f, g, board, blank_pos, path)
+    
+    heap = [start_state]
+    visited = set()
 
-    while open_list:
-        current_state = heapq.heappop(open_list)
-        if current_state.board == goal_state:
-            return current_state
+    while heap:
+        f, g, board, blank, path = heapq.heappop(heap)
 
-        closed_list.add(tuple(current_state.board))
-        blank_pos = current_state.board.index(0)
+        if board == GOAL:
+            return path + [board]
 
-        for move in moves:
-            if move == 'U' and blank_pos < 3:  # Invalid move up
-                continue
-            if move == 'D' and blank_pos > 5:  # Invalid move down
-                continue
-            if move == 'L' and blank_pos % 3 == 0:  # Invalid move left
-                continue
-            if move == 'R' and blank_pos % 3 == 2:  # Invalid move right
+        visited.add(tuple(board))
+
+        for direction in DIRECTIONS:
+            if not is_valid_move(blank, direction):
                 continue
 
-            new_board = move_tile(current_state.board, move, blank_pos)
-
-            if tuple(new_board) in closed_list:
+            new_board, new_blank = move(board, direction, blank)
+            if tuple(new_board) in visited:
                 continue
 
-            new_state = PuzzleState(new_board, current_state, move, current_state.depth + 1, current_state.depth + 1 + heuristic(new_board))
-            heapq.heappush(open_list, new_state)
+            h = manhattan(new_board)
+            heapq.heappush(heap, (g + 1 + h, g + 1, new_board, new_blank, path + [board]))
+
     return None
 
-def print_solution(solution):
-    path = []
-    current = solution
-    while current:
-        path.append(current)
-        current = current.parent
-    path.reverse()
+# Print board
+def print_board(board):
+    for i in range(0, 9, 3):
+        row = board[i:i+3]
+        print(' '.join(str(x) if x != 0 else ' ' for x in row))
+    print()
 
-    for step in path:
-        print(f"Move: {step.move}")
-        print_board(step.board)
+# Run
+initial = [1, 2, 3,
+           4, 0, 5,
+           6, 7, 8]
 
-initial_state = [1, 2, 3, 4, 0, 5, 6, 7, 8]
-solution = a_star(initial_state)
+solution = a_star(initial)
+
 if solution:
-    print(colored("Solution found:", "green"))
-    print_solution(solution)
+    print("Solution found! Steps:")
+    for step, board in enumerate(solution):
+        print(f"Step {step}:")
+        print_board(board)
+    print("Final Goal State:")
+    print_board(GOAL)
 else:
-    print(colored("No solution exists.", "red"))
+    print("No solution found.")
